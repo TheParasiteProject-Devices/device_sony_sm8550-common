85df8f276d2e0e4e4a26f381319c8b249563ee3e
Chris Fries <cfries@google.com>
2019-02-25 20:59:08 +0000

    Set VENDOR_SECURITY_PATCH to platform
    Set the VENDOR_SECURITY_PATCH level to match the platform
    security patch level.  These are functionally the same.
    
    Bug: 125914443
    Change-Id: Ib77ab115d6faabd21958c34e2dae49ec96421fdd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -243,7 +243,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
 BOOT_SECURITY_PATCH := 2024-03-01
-VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
