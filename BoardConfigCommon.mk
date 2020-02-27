352e5c6d316641cc7afd97631688557bb6dc3376
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    sm8550-common: Panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ BOARD_KERNEL_CMDLINE += nosoftlockup
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
+    androidboot.init_fatal_reboot_target=recovery \
     androidboot.usbcontroller=a600000.dwc3
 
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
