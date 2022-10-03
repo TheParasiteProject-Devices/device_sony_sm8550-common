9f3b4add038c3bcf87a2a32a86a2f194cf783844
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-03 23:19:34 +0530

    sm8550-common: rootdir: Import cgroup tuning from gs101
    Inspired by
    https://github.com/PixelOS-Devices/device_xiaomi_lisa/commit/936b04daff511c21f5a6ea9b201cfd5ef3013bc8
    
    Change-Id: I01825812ba9898e142ef12ca8bcceef7d7a3e34a
    Signed-off-by: bheatleyyy <sugakesshaa@pixelos.net>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- rootdir/init.kalama.post_boot.sh
+++ rootdir/init.kalama.post_boot.sh
@@ -5,5 +5,10 @@
 
 # Custom tuning for Lahaina SoC
 
+# uclamp tuning
+echo 50 > /dev/cpuctl/background/cpu.uclamp.max
+echo 50 > /dev/cpuctl/system-background/cpu.uclamp.max
+echo 60 > /dev/cpuctl/dex2oat/cpu.uclamp.max
+
 # Enable suspend to RAM
 write /sys/power/mem_sleep "deep"
