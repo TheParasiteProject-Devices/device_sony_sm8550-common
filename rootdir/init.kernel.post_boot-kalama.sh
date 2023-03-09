b0075b862db717b466f381e8f0e2a79d8ba765a0
Jake Weinstein <jake@aospa.co>
2023-03-10 07:58:57 +0900

    sm8550-common: rootdir: Do not allow foreground tasks to run on CPU7
    Gold+ cores use significantly more power than Gold cores.
    Only allow top-app tasks to run on Gold+ to save power
    and prioritize the most critical threads.
    
    Change-Id: I95e5e18dfe0e5b6818eee1c589a22959bea4868a

--- rootdir/init.kernel.post_boot-kalama.sh
+++ rootdir/init.kernel.post_boot-kalama.sh
@@ -115,6 +115,7 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-2 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Set restricted cpuset to the same CPUs as system-background
 cat /dev/cpuset/system-background/cpus > /dev/cpuset/restricted/cpus
