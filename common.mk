8b361dd1d057c79609902e7b10653cdf23eeac03
someone5678 <nemui3353@gmail.com>
2023-08-21 22:26:53 +0900

    sm8550-common: Remove LineageOS specific stuffs
    * Remove LineageOS specific overlays
      and move lineage-sdk overlay to frameworks
    * Remove dependancies list
    * Remove Lineage Health
    * Remove WITH_GMS flags
    
    Change-Id: I588d240a54d9736cdd346895d208d7e9b9a593d0

--- common.mk
+++ common.mk
@@ -349,10 +349,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Lineage Health
-# PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
