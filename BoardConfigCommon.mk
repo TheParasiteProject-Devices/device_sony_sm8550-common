f3f2ec36c76b014cf0a20af42356643b467d68e7
someone5678 <nemui3353@gmail.com>
2023-08-23 12:22:08 +0900

    sm8550-common: Include LineageOS' device FCM from this project
    Change-Id: I16d697625766c8c2611724775f53947e2775d52c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,7 +215,7 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED := 1
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
