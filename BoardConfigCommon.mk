79f372dc2e3a18e9fcd659ad817430e339288d08
someone5678 <nemui3353@gmail.com>
2023-08-25 18:59:53 +0900

    sm8550-common: BoardConfig: vintf: Use += instead of := for inclusion
    * So that we can include manifests files outside of device tree
    
    Change-Id: I32f868da565c21a9d97688b36ae2855ba6b71146

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,8 +164,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/sony/vintf/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := \
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/manifest.xml \
     $(COMMON_PATH)/network_manifest.xml \
     hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
