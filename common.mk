e7658779de00789e3ffb2f951fdaa997144a2af6
someone5678 <nemui3353@gmail.com>
2023-08-25 13:44:22 +0900

    sm8550-common: props: Import LMKD setups
    Change-Id: I4765444e5d0a4989c9853f33c80b9d9350177edb

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ PRODUCT_PACKAGES += \
 # ART optimizations
 $(call inherit-product, $(LOCAL_PATH)/art.mk)
 
+# LMKD setups
+$(call inherit-product, $(LOCAL_PATH)/lmkd.mk)
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
