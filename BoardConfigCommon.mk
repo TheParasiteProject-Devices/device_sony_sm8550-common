fc47011576ca3be9439816b06adec2032ec2a1da
someone5678 <someone5678@users.noreply.github.com>
2023-11-13 22:29:41 +0900

    sm8550-common: BoardConfig: Set loop.max_part to 8
    Ref:
    https: //source.android.com/docs/core/ota/apex#kernel-command-line
    Change-Id: I97c63ec315a99a184a7fcab7c650a3899128c91c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,7 @@ BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.init_fatal_reboot_target=recovery \
+    loop.max_part=8 \
     androidboot.usbcontroller=a600000.dwc3
 
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
