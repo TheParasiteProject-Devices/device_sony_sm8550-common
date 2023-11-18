9d21010fac78d81a8de0d3a7e884d7e0ee8b5796
someone5678 <someone5678@users.noreply.github.com>
2023-11-18 23:25:23 +0900

    Revert "sm8550-common: Include LineageOS' device FCM from this project"
    This reverts commit 726c35e499dd6fad27120a01308a1c3e4b2a1617.
    
    Change-Id: Ie98b5819dc96eca73f153a997e7fb6da4109fe3b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -219,8 +219,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/configs/vintf/manifest.xml \
