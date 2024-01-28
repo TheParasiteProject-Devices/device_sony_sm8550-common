58c247d6564a4b7049cd748faccbf988b220699e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 22:10:39 +0530

    sm8550-common: Dynamically set IRQ affinity
    - Introduce a new shell script to set IRQ affinity, since different
      kernels can have different IRQ numbers
    
    Change-Id: I8fed1fc22f206d368ce3daf587af0086032feb78

--- rootdir/init.kalama.post_boot.sh
+++ rootdir/init.kalama.post_boot.sh
@@ -1,9 +1,19 @@
+#!/vendor/bin/sh
 #
 # Copyright (C) 2023 StatiXOS
+# Copyright (C) 2024 Paranoid Android
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Custom tuning for Lahaina SoC
+
+function write_irq_affinity() {
+    # Arguments:
+    # $1 = irq name
+    # $2 = cpu id
+    irq_dir="$(dirname /proc/irq/*/$1)"
+    [ -d "$irq_dir" ] && echo $2 > "${irq_dir}/smp_affinity_list"
+}
 
 # uclamp tuning
 echo 50 > /dev/cpuctl/background/cpu.uclamp.max
@@ -12,3 +22,9 @@ echo 60 > /dev/cpuctl/dex2oat/cpu.uclamp.max
 
 # Enable suspend to RAM
 write /sys/power/mem_sleep "deep"
+
+# IRQ Tuning
+# kgsl_3d0_irq -> CPU 6
+# msm_drm -> CPU 7
+write_irq_affinity kgsl_3d0_irq 6
+write_irq_affinity msm_drm 7
