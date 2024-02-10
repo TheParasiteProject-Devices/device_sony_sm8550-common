cb14bf70d85277d257ae3c2dbe2e7c330b88b719
someone5678 <someone5678@users.noreply.github.com>
2024-02-11 02:06:14 +0900

    sm8550-common: Build more bluetooth packages
    * Match stock
    
    Change-Id: I2e5873c34cd3e0f64dfc452843402ef78e1ef3a8

--- common.mk
+++ common.mk
@@ -99,9 +99,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-V2-ndk.vendor \
     vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
