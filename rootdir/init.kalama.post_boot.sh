8ced1cd5e54b371f19020d0d38b7991f26c73b9a
someone5678 <someone5678@users.noreply.github.com>
2024-02-19 19:24:50 +0900

    Revert "sm8550-common: rootdir: Enable suspend to RAM"
    * Does not works correctly on 5.15
    
    This reverts commit e25777fb698ab604643df18e3eeabc2d5217a08b.
    
    Change-Id: Ib470efefe59146f88320ec0b3be0677ab93f112e

--- rootdir/init.kalama.post_boot.sh
+++ rootdir/init.kalama.post_boot.sh
@@ -20,9 +20,6 @@ echo 50 > /dev/cpuctl/background/cpu.uclamp.max
 echo 50 > /dev/cpuctl/system-background/cpu.uclamp.max
 echo 60 > /dev/cpuctl/dex2oat/cpu.uclamp.max
 
-# Enable suspend to RAM
-write /sys/power/mem_sleep "deep"
-
 # IRQ Tuning
 # kgsl_3d0_irq -> CPU 6
 # msm_drm -> CPU 7
