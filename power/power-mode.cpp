773feb2e00e508af32044e5a382e989cb231c908
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    sm8550-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -21,10 +21,11 @@
 using ::aidl::android::hardware::power::Mode;
 
 namespace aidl {
-namespace android {
+namespace google {
 namespace hardware {
 namespace power {
 namespace impl {
+namespace pixel {
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
@@ -52,5 +53,6 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
 }  // namespace impl
 }  // namespace power
 }  // namespace hardware
-}  // namespace android
+}  // namespace google
 }  // namespace aidl
+}  // namespace pixel
