b1a94a226db04dc70a609fa3a42ac962595dde23
Luofan Chen <clfbbn@gmail.com>
2024-04-24 21:53:08 +0800

    sm8550-common: Build vendor.qti.hardware.display.config-V5-ndk
    Used for WFD
    
    Change-Id: I9df31fa04eb5a8293b94859a9f7a4cd54e8ed46d

--- common.mk
+++ common.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.config-V5-ndk \
     vendor.qti.hardware.display.config-V1-ndk.vendor \
     vendor.qti.hardware.display.config-V2-ndk.vendor \
     vendor.qti.hardware.display.config-V3-ndk.vendor \
