6ceebef53c4f11be38823cc7887fcff57338189c
someone5678 <someone5678@users.noreply.github.com>
2024-04-29 17:58:56 +0900

    sm8550-common: Setup additional options for ota
    * Enable --enable_lz4diff and --enable_vabc_xor options to make ota much smaller
    
    Required commit:
    https://github.com/TheParasiteProject/build/commit/c34016e57e8957313365196a3812d58251c4d6b6
    
    Change-Id: I6c81d35e56b3725beda3d54e33e19a2974712e40

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,10 @@ AB_OTA_PARTITIONS += \
     vendor_dlkm \
     vendor_boot
 
+TARGET_OTA_ADDITIONAL_OPTIONS := \
+    --enable_lz4diff=true \
+    --enable_vabc_xor=true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv9-a
