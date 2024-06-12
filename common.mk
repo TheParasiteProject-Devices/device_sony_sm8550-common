b7870e99f1ea4274edff183738a0284ab1e3824e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm8550-common: Setup DeviceAsWebcam
    * USB VID & PID are taken from gs101 UsbGadget.cpp
    * bDeviceClass related entries are taken from init.qcom.usb.rc
    
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- common.mk
+++ common.mk
@@ -380,6 +380,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    SonyYodoDeviceAsWebcamResCommon \
     SonyYodoFrameworksResCommon \
     SonyYodoFrameworksResParasite \
     SonyYodoSettingsProviderOverlayCommon \
