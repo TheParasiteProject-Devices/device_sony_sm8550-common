2667692970da7927a090c64064603977ffe1352c
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 20:47:08 +0900

    sm8550-common: vendorsetup.sh: Correct var name
    Change-Id: Id077f5014b0266f4148a22c8697583809fd1deff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,14 +2,14 @@
 
 if [[ $(echo -n $TARGET_PRODUCT | sed -e 's/^aosp_//g') = "pdx234" ]]; then
 
-if [ -z $VENDOR_SONY_SM8550_SETUP_DONE ]; then
+if [ -z $DEVICE_SONY_SM8550_SETUP_DONE ]; then
 
 cd vendor/qcom/opensource/usb
 git reset --hard
 git apply "../../../../device/sony/sm8550-common/.patch/0001-TMP-Import-sony-changes.patch"
 cd ../../../../
 
-export VENDOR_SONY_SM8550_SETUP_DONE=true
+export DEVICE_SONY_SM8550_SETUP_DONE=true
 fi
 
 fi
