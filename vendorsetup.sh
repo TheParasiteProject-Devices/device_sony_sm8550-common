23ffe82eac4ad41de11794f9d62c41455d08b42d
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 19:32:12 +0900

    sm8550-common: vendorsetup.sh: Guard it
    Change-Id: I03e8d8538cc3375d3206c9847b538bd707147d0c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ $(echo -n $TARGET_PRODUCT | sed -e 's/^aosp_//g') = "pdx234" ]]; then
+
 if [ -z $VENDOR_SONY_SM8550_SETUP_DONE ]; then
 
 cd vendor/qcom/opensource/usb
@@ -8,4 +10,6 @@ git apply "../../../../device/sony/sm8550-common/.patch/0001-TMP-Import-sony-cha
 cd ../../../../
 
 export VENDOR_SONY_SM8550_SETUP_DONE=true
+fi
+
 fi
