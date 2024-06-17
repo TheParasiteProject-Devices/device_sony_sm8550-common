1c149a9df6534efa00e5f8ea251e30272093194b
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 20:05:32 +0900

    sm8550-common: Enable blur
    * We added config for blur
    
    Change-Id: Ibdf6a33501029e304772db5bf7d92ae9caadf4b6

--- common.mk
+++ common.mk
@@ -610,6 +610,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # XperiaModules
 $(call inherit-product-if-exists, hardware/sony/XperiaModules.mk)
 
