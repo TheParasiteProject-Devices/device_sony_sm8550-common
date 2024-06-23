4e476c3f5f96190363f4760d0ef17c17c77ee842
someone5678 <someone5678@users.noreply.github.com>
2024-06-24 00:01:20 +0900

    sm8550-common: Build libidd dummy
    * Avoid binaries crashes due to missing libidd
      while make us enable to gutted out in device diagnostics
    
    Change-Id: I280d1dcc7ed9957a56a9069bd675dea4c5ee96f6

--- common.mk
+++ common.mk
@@ -486,6 +486,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/sony \
+    hardware/sony/libidd \
     kernel/sony/sm8550 \
     kernel/sony/sm8550-modules
 
@@ -623,6 +624,10 @@ PRODUCT_COPY_FILES += \
 
 # Blur
 TARGET_ENABLE_BLUR := true
+
+# In Device Diagnostics (Dummy)
+PRODUCT_PACKAGES += \
+    libidd
 
 # XperiaModules
 $(call inherit-product-if-exists, hardware/sony/XperiaModules.mk)
