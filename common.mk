5abf556aa39125cf5993f66d002a8495965ea9a3
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    sm8550-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- common.mk
+++ common.mk
@@ -7,6 +7,9 @@
 # Signing build
 TARGET_BUILD_FULLY_SIGN := true
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
