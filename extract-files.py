853715f2e85257e986c5bf7f38006fdf6a383dbe
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    sm8550-common: Use libhidlbase_shim for vendor.libdpmframework
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- extract-files.py
+++ extract-files.py
@@ -96,6 +96,10 @@ blob_fixups: blob_fixups_user_type = {
     .replace_needed(
         'android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'
     ),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+    .add_needed(
+        'libhidlbase_shim.so',
+    ),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
