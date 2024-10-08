7b35121b100657f5290f289c165d1e6c6cea56fd
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-08 21:18:38 +0900

    sm8550-common: Add dummy libqti-perfd-client to namespace
    Change-Id: I373188dbc2130753ae9213b6f394f3fafe227fb9

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,6 +24,7 @@ source "${HELPER}"
 
 function vendor_imports() {
     cat <<EOF >>"$1"
+        "hardware/qcom-caf/common/libqti-perfd-client",
         "hardware/qcom-caf/sm8550",
         "hardware/qcom-caf/wlan",
         "hardware/sony",
