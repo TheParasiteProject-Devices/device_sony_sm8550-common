2274ac9e833c4c4a9dc96d02ed5c809a9cf7e493
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8550-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- common.mk
+++ common.mk
@@ -279,6 +279,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    NcmTetheringOverlay \
     SimDualResCommon \
     SimSingleResCommon \
     SonyYodoDeviceAsWebcamResCommon \
