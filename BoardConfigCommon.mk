f1e0eae4a8fae48b0db7e44c60e7c783acedc36e
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-17 20:34:52 +0900

    sm8550-common: Put vendor/sony/kalama_GKI.config last
    * Always overrides other configs
    
    Change-Id: Icfc7e7c9b213657c403dc4df1cc7d090ee79c16d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,8 +96,8 @@ TARGET_KERNEL_SOURCE := kernel/sony/sm8550
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/kalama_GKI.config \
-    vendor/sony/kalama_GKI.config \
-    vendor/debugfs.config
+    vendor/debugfs.config \
+    vendor/sony/kalama_GKI.config
 
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
