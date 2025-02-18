d17ec6705c75cbaa2cbad52ea83802e69752e553
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-18 11:03:55 +0200

    sm8550-common: Switch to AIDL NXP NFC HAL
    Let nqnfcinfo read the Chip ID, start the HAL after setting the proper
    configuration file based on the Chip ID.
    
    0xc1 is SN220, 0xca is PN560, 0x41 is PN557, 0xa4 is SN100, found
    by reverse engineering the CNfcConfig::getconfiguration_id() function.
    
    Only SN220 and PN560 seem used on bronco.
    
    There is no difference between the PN560 and SN220 system config.
    
    Change-Id: Iff03bd7fc522b45b9caebb12860c5c851356a9a1

--- common.mk
+++ common.mk
@@ -360,8 +360,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.2.vendor \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     libchrome.vendor \
     Tag \
