LineageOS/android_device_oneplus_sm8550-common

sm8550-common: Switch to Touch AIDL
Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

common.mk
@@ -345,7 +345,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch-service.oplus
 
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
 

TheParasiteProject-Devices/device_sony_sm8550-common

common.mk
@@ -395,7 +395,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sony
+    vendor.lineage.touch-service.sony
 
 # Update engine
 PRODUCT_PACKAGES += \