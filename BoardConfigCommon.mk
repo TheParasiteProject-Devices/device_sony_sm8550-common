7a409dbc010cb60f141e5a680b1160702adb9a53
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    sm8550-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,10 +160,9 @@ TARGET_USES_QCOM_MM_AUDIO := true
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/sony/vintf/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/sony/vintf/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
