6ed022b0aa21290d92e4f008f0b396d11c10e174
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-09-07 03:36:40 +0900

    fixup! sm8550-common: dolby: Setup overlay

--- common.mk
+++ common.mk
@@ -281,8 +281,8 @@ $(call soong_config_set,media,needs_mutexlock_disabled_for_vtservice,true)
 
 # Media (Dolby)
 PRODUCT_PACKAGES += \
-    LineageDolby \
-    SonyYodoLineageDolbyResCommon
+    ParanoidDolby \
+    SonyYodoParanoidDolbyResCommon
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
