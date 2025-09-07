aospa-opace3/device_oneplus_sm8550-common

sm8550-common: Stop including kernel and modules namespaces
This looks like a bringup leftover and is not necessary.

Change-Id: If298a691a8ca114d75016c3f04ca50547f9786d0

common.mk
@@ -318,9 +318,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus \
-    kernel/oneplus/sm8550 \
-    kernel/oneplus/sm8550-modules
+    hardware/oplus
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)

TheParasiteProject-Devices/device_sony_sm8550-common

common.mk
@@ -341,9 +341,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/sony \
-    kernel/sony/sm8550 \
-    kernel/sony/sm8550-modules
+    hardware/sony
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)