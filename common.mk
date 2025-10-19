0078db9e9c7293e2fe37e3b79f72f62df03a7408
Luofan Chen <clfbbn@gmail.com>
2025-10-19 17:06:32 +0900

    sm8550-common: Build LiveDisplay AIDL
    Change-Id: I4befcafcd58787a3d3564a2a1445b9fbe52daa8b

--- common.mk
+++ common.mk
@@ -237,6 +237,12 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
