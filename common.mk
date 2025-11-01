0474cd04cd5ee4e62c2e8b5ea3476216e43764f0
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-11-01 16:52:36 +0900

    sm8550-common: power: Enable interaction boost
    Change-Id: I7276cbf70b65620a3f15b81bd2047a23171a3e66

--- common.mk
+++ common.mk
@@ -350,6 +350,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+$(call soong_config_set,qtipower,interaction_boost,true)
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
 
 # PowerShare
