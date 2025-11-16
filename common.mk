4f038138eff656937a80091203d2dd3061d5ccda
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    sm8550-common: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0

--- common.mk
+++ common.mk
@@ -98,6 +98,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 $(call soong_config_set,libaudiopolicyengine,prioritize_builin_over_back_mic,true)
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Authsecret
 PRODUCT_PACKAGES += \
