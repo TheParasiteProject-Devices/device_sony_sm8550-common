90569766f8411e0d434bb09bdfdeabd7faa14b12
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-12-11 19:21:57 +0900

    fixup! sm8550-common: Opt out of speaker_layout_channel_mask field

--- common.mk
+++ common.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 $(call soong_config_set_bool,libaudiopolicyengine,prioritize_builin_over_back_mic,true)
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Authsecret
 PRODUCT_PACKAGES += \
