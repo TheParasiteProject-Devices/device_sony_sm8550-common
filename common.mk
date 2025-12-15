LineageOS/android_device_oneplus_sm8550-common

sm8550-common: Rename manifest file to follow QCOM naming convention
Change-Id: I888147c059a4abbb6936c2544299b98f3474416f

common.mk
@@ -411,7 +411,7 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
-    $(LOCAL_PATH)/manifest.xml \
+    $(LOCAL_PATH)/manifest_kalama.xml \
     $(LOCAL_PATH)/network_manifest.xml
 
 ODM_MANIFEST_FILES := \

TheParasiteProject-Devices/device_sony_sm8550-common

common.mk
@@ -481,7 +481,7 @@ DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
-    $(COMMON_PATH)/manifest.xml \
+    $(COMMON_PATH)/manifest_kalama.xml \
     $(COMMON_PATH)/network_manifest.xml
 
 # WiFi
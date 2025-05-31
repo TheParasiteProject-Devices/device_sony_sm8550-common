ed88dbab2b1a41a77a8b99076770db137ff52210
Bruno Martins <bgcngm@gmail.com>
2025-05-31 13:19:22 +0100

    sm8550-common: Rename manifest file to follow QCOM naming convention
    Change-Id: I888147c059a4abbb6936c2544299b98f3474416f

--- common.mk
+++ common.mk
@@ -481,7 +481,7 @@ DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
-    $(COMMON_PATH)/manifest.xml \
+    $(COMMON_PATH)/manifest_kalama.xml \
     $(COMMON_PATH)/network_manifest.xml
 
 # WiFi
